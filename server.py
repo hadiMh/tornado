45a0b154474a794dc51ad172bf24fadae422cff5
hadi <hadih7899@gmail.com>
2019-04-07 18:31:06 +0430

    added all post methods to all of the classes. the arrays uses and the unused methods are all commented.all this kind of comments should be cleared

--- server.py
+++ server.py
@@ -12,23 +12,23 @@ import mydb
 import json
 import collections
 
-admins = [
-    {
-        "username": "hadi"
-    }
-]
-
-users = [
-    {
-        "username": "hadi",
-        "password": "1234",
-        "firstname": "hadi",
-        "lastname": "",
-        "token": ""
-    }
-]
-
-tickets = []
+# admins = [
+#     {
+#         "username": "hadi"
+#     }
+# ]
+#
+# users = [
+#     {
+#         "username": "hadi",
+#         "password": "1234",
+#         "firstname": "hadi",
+#         "lastname": "",
+#         "token": ""
+#     }
+# ]
+#
+# tickets = []
 
 def createUserTicketList(userTicketList):
     result = {}
@@ -60,20 +60,30 @@ def getQueryParametes(self, parameters):
             returnValues.append(0)
     return returnValues
 
-def getUserByToken(token):
-    for item in users:
-        if(item["token"] == token):
-            return item
-    return 0
+def getPostParameters(self, parameters):
+    returnValues = []
+    for value in parameters:
+        result = self.get_argument(value)
+        if(result):
+            returnValues.append(result)
+        else:
+            returnValues.append(0)
+    return returnValues
+
+# def getUserByToken(token):
+#     for item in users:
+#         if(item["token"] == token):
+#             return item
+#     return 0
 
 # global methods
-def findUser(username):
-    for user in users:
-        print("user['username']:", user["username"])
-        print("username:", username)
-        if(user["username"] == username):
-            return user
-    return -1
+# def findUser(username):
+#     for user in users:
+#         print("user['username']:", user["username"])
+#         print("username:", username)
+#         if(user["username"] == username):
+#             return user
+#     return -1
 
 def generateRandomToken():
     return hexlify(os.urandom(16)).decode('utf-8')
@@ -88,47 +98,41 @@ def getUserLoginToken(username):
             mydb.saveTokenToThisUser(username, user['token'])
         return user['token']
 
-def changeTicketStatus(tickets, ticketId, newStatus):
-    for item in tickets:
-        if '%d'%item["id"] == ticketId:
-            item["status"] = newStatus
-            return 1
-    return 0
+# def changeTicketStatus(tickets, ticketId, newStatus):
+#     for item in tickets:
+#         if '%d'%item["id"] == ticketId:
+#             item["status"] = newStatus
+#             return 1
+#     return 0
 
-def isThisUsernameAdmin(username):
-    for admin in admins:
-        if(admin["username"] == username):
-            return True
-    return False
+# def isThisUsernameAdmin(username):
+#     for admin in admins:
+#         if(admin["username"] == username):
+#             return True
+#     return False
 
-def getUserAllTickets(username):
-    userAllTickets = []
-    admin = False
-    if isThisUsernameAdmin(username):
-        admin = True
-    for item in tickets:
-        if admin or item["username"] == username:
-            userAllTickets.append(item)
+# def getUserAllTickets(username):
+#     userAllTickets = []
+#     admin = False
+#     if isThisUsernameAdmin(username):
+#         admin = True
+#     for item in tickets:
+#         if admin or item["username"] == username:
+#             userAllTickets.append(item)
+#
+#     return userAllTickets
 
-    return userAllTickets
-
-def saveThisResponseForThisTicket(ticketId, body):
-    for item in tickets:
-        if '%d'%item["id"] == ticketId:
-            item["response"] = body
-            item["status"] = "Closed"
-            return True
-    return False
+# def saveThisResponseForThisTicket(ticketId, body):
+#     for item in tickets:
+#         if '%d'%item["id"] == ticketId:
+#             item["response"] = body
+#             item["status"] = "Closed"
+#             return True
+#     return False
 
 # request handler classes
 class MyRequestHandler(RequestHandler):
     @property
-
-    def ifUserAlreadyExists(self, username):
-        for item in users:
-            if(item["username"] and item["username"] == username):
-                return True
-        return False
 
     def hello(self):
         print("second function")
@@ -162,21 +166,68 @@ class SignupHandler(MyRequestHandler):
 
         if not userExists:
             mydb.createUserInUsersTable(username, password, firstname, lastname)
-            users.append(
-                {
-                    "username": username,
-                    "password": password,
-                    "firstname": firstname,
-                    "lastname": lastname,
-                    "token": ""
-                }
-            )
+            # users.append(
+            #     {
+            #         "username": username,
+            #         "password": password,
+            #         "firstname": firstname,
+            #         "lastname": lastname,
+            #         "token": ""
+            #     }
+            # )
             self.write({
                 "message": "Signed Up Successfully",
                 "code": "200"
             })
         else:
-            self.write("user already exists")
+            self.write(
+                {
+                    "message": "user already exists"
+                }
+            )
+
+    def post(self, *args, **kwargs):
+        username, password, firstname, lastname = getPostParameters(self,
+                                                                    ['username', 'password', 'firstname', 'lastname'])
+        ## username = re.search("(?<=username=)([^&]+)?", self.request.uri)
+        ## password = re.search("(?<=password=)([^&]+)?", self.request.uri)
+        ## firstname = re.search("(?<=firstname=)([^&]+)?", self.request.uri)
+        ## lastname = re.search("(?<=lastname=)([^&]+)?", self.request.uri)
+
+        if (not username or not password):
+            self.write({"message:": "username and password are required"})
+            return
+
+        if (not firstname):
+            firstname = ""
+
+        if (not lastname):
+            lastname = ""
+
+        # check if user already exists in the saved users
+        userExists = mydb.doesThisUserAlreadyExist(username)
+
+        if not userExists:
+            mydb.createUserInUsersTable(username, password, firstname, lastname)
+            # users.append(
+            #     {
+            #         "username": username,
+            #         "password": password,
+            #         "firstname": firstname,
+            #         "lastname": lastname,
+            #         "token": ""
+            #     }
+            # )
+            self.write({
+                "message": "Signed Up Successfully",
+                "code": "200"
+            })
+        else:
+            self.write(
+                {
+                    "message": "user already exists"
+                }
+            )
 
 class LoginHandler(MyRequestHandler):
     def get(self, *args):
@@ -185,6 +236,43 @@ class LoginHandler(MyRequestHandler):
 
         # if username or password are not in the request
         if(not username or not password):
+            self.write({
+                "message": "username and password are required."
+            })
+            return
+
+        # find the user if exist
+        user = mydb.doesThisUserAlreadyExist(username)
+        # if user doesnt exist, tell it to response
+        if not user:
+            self.write({
+                "message": "user doesn't exist in the database."
+            })
+            return
+        print("user", user)
+        # if user exist and the password is correct
+        if (user) and user["password"] == password:
+            # the below function, saves the token in the user data
+            token = getUserLoginToken(username=username)
+            self.write({
+                "message": "Logged in Successfully",
+                "code": "200",
+                "token": token
+            })
+        else:
+            # if the password is not correct
+            self.write({
+                "message": "username or password is not correct"
+            })
+
+    def post(self, *args, **kwargs):
+        # get username and password that the request contains
+        username, password = getPostParameters(self, ['username', 'password'])
+        # username = self.get_argument('username')
+        # password = self.get_argument('password')
+        print("post parameters", username, password)
+        # if username or password are not in the request
+        if (not username or not password):
             self.write({
                 "message": "username and password are required."
             })
@@ -247,6 +335,38 @@ class LogoutHandler(MyRequestHandler):
                 "message": "username or password is not correct"
             })
 
+    def post(self, *args, **kwargs):
+        username, password = getPostParameters(self, ['username', 'password'])
+        if (not username or not password):
+            self.write({
+                "message": "username and password are required."
+            })
+            return
+        # find the user if exist
+        user = mydb.clearUserToken(username, password)
+        # if user doesnt exist, tell it to response
+        if not user:
+            self.write({
+                "message": "user have already logged out or "
+                           "user doesn't exist in the database or "
+                           "your password is not correct"
+            })
+            return
+
+        # if user exist and the password is correct
+        if (user):
+            # clear the token so the user is logged out
+            self.write({
+                "message": "Logged Out Successfully",
+                "code": "200"
+            })
+            return
+        else:
+            # if the password is not correct
+            self.write({
+                "message": "username or password is not correct"
+            })
+
 class SendTicketHandler(MyRequestHandler):
     def get(self, *args):
         token, subject, body = getQueryParametes(self, ['token', 'subject', 'body'])
@@ -277,9 +397,63 @@ class SendTicketHandler(MyRequestHandler):
              "code": "200"
         })
 
+    def post(self, *args, **kwargs):
+        token, subject, body = getPostParameters(self, ['token', 'subject', 'body'])
+        if (not token):
+            self.write({
+                "message": "token is required. please login first."
+            })
+            return
+        if (not subject or not body):
+            self.write({
+                "message": "subject and body of the ticket are required."
+            })
+            return
+
+        user = mydb.getUserByToken(token)
+
+        if (not user):
+            self.write({
+                "message": "token not valid"
+            })
+            return
+
+        ticketId = mydb.saveTicket(user['username'], subject, body)
+
+        self.write({
+            "message": "Ticket Sent Successfully",
+            "id": ticketId,
+            "code": "200"
+        })
+
 class UserGetTicketHandler(MyRequestHandler):
     def get(self, *args):
         token = getQueryParametes(self, ['token'])[0]
+        if(not token):
+            self.write({
+                "message": "request in correct format"
+            })
+            return
+
+        user = mydb.getUserByToken(token)
+
+        if not user:
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        userTickets = mydb.getAllUserTickets(user['username'])
+
+        numberOfTickets = len(userTickets)
+        correctFormatUserTicketsList = createUserTicketList(userTickets)
+
+        self.write(
+            collections.OrderedDict(sorted(correctFormatUserTicketsList.items()))
+        )
+
+    def post(self, *args):
+        token = getPostParameters(self, ['token'])[0]
         if(not token):
             self.write({
                 "message": "request in correct format"
@@ -321,6 +495,23 @@ class UserCloseTicketHandler(MyRequestHandler):
             })
             return
 
+    def post(self, *args, **kwargs):
+        token, id = getPostParameters(self, ['token', 'id'])
+        user = mydb.getUserByToken(token)
+        userTickets = mydb.getAllUserTickets(user["username"])
+        if(mydb.changeTicketStatus(id, "Closed")):
+            self.write({
+                "message": "Ticket With id -%s- Closed Successfully" % id,
+                "code": "200"
+            })
+            return
+        else:
+            self.write({
+                "message": "No such user or ticket",
+                "code": "404"
+            })
+            return
+
 class AdminGetAllTicketsHandler(MyRequestHandler):
     def get(self, *args):
         token, = getQueryParametes(self, ['token'])
@@ -346,9 +537,70 @@ class AdminGetAllTicketsHandler(MyRequestHandler):
             collections.OrderedDict(sorted(correctFormatUserTicketsList.items()))
         )
 
+    def post(self, *args, **kwargs):
+        token, = getPostParameters(self, ['token'])
+        if (not token):
+            self.write({
+                "message": "request in correct format"
+            })
+            return
+
+        user = mydb.getUserByToken(token)
+        print("isthisuseradmin",mydb.isThisTokenAdmin(token))
+        if not user or not mydb.isThisTokenAdmin(token):
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        userTickets = mydb.getAllUserTickets(user["username"])
+        numberOfTickets = len(userTickets)
+        correctFormatUserTicketsList = createUserTicketList(userTickets)
+
+        self.write(
+            collections.OrderedDict(sorted(correctFormatUserTicketsList.items()))
+        )
+
 class AdminAnswerToTicketHandler(MyRequestHandler):
     def get(self, *args):
         token, id, body = getQueryParametes(self, ['token', 'id', 'body'])
+        if(not token):
+            self.write({
+                "message": "token is required"
+            })
+            return
+        if(not id or not body):
+            self.write({
+                "message": "the ticket id and the response text are required"
+            })
+            return
+
+        user = mydb.getUserByToken(token)
+        if not user:
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        if not mydb.isThisTokenAdmin(token):
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        if mydb.doesThisTicketExists(id) and mydb.saveThisResponseForThisTicket(id, body):
+            self.write({
+                "message": "Response to Ticket With id -%s- Sent Successfully" % id,
+                "code": "200"
+            })
+        else:
+            self.write({
+                "message": "Response to Ticket With id -%s- Was not Successfully. Please get sure for ticket existence." % id,
+                "code": "200"
+            })
+
+    def post(self, *args, **kwargs):
+        token, id, body = getPostParameters(self, ['token', 'id', 'body'])
         if(not token):
             self.write({
                 "message": "token is required"
@@ -424,9 +676,50 @@ class AdminChangeTicketStatus(MyRequestHandler):
             })
             return
 
+    def post(self, *args, **kwargs):
+        token, id, status = getPostParameters(self, ['token', 'id', 'status'])
+        if (not token):
+            self.write({
+                "message": "token is required"
+            })
+            return
+        if (not id or not status):
+            self.write({
+                "message": "the ticket id and the status are required"
+            })
+            return
+
+        user = mydb.getUserByToken(token)
+        if not user:
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        if not mydb.isThisTokenAdmin(token):
+            self.write({
+                "message": "token is not valid"
+            })
+            return
+
+        if  mydb.doesThisTicketExists(id) and mydb.changeTicketStatus(id, status):
+            self.write({
+                "message": "Status Ticket With id -%s- Changed Successfully" % id,
+                "code": "200"
+            })
+            return
+        else:
+            self.write({
+                "message": "No such ticket or user",
+                "code": "404"
+            })
+            return
+
 class DefaultHandler(MyRequestHandler):
     def get(self, *args):
-        self.write("default handler")
+        self.write({
+            "message": "page does not exist"
+        })
         print(self.request)
 
 def make_app():
